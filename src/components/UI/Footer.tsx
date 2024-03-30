ea17a525f06b5d3a42cf09bb43c67a398dac6173
riteshsinghh-coder <riteshkumar26563@gmail.com>
2024-03-30 15:23:18 +0530

    error solved

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -6,12 +6,12 @@ export const Footer = () => {
   return (
     <Flex as='footer' align='center' p={1}>
       <Text>made by</Text>
-      <Link href={'https://github.com/Kirill2603'} target='_blank'>
+      <Link href={'https://github.com/riteshsinghh-coder'} target='_blank'>
         <Button
           size='xs'
           variant='ghost'
           colorScheme='teal'
-          leftIcon={<Icon as={FaGithub} />}>@Kirill2603</Button>
+          leftIcon={<Icon as={FaGithub} />}>@riteshsinghh-coder</Button>
       </Link>
     </Flex>
   )
